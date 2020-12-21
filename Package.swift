2bbe1abef1231dda82221052be57a144148d8d90
Alessandro Marcon <a.marcon@vetrya.com>
2020-12-21 19:43:00 +0100

    Trying to adding resources to SPM

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HermesAlert",
-            exclude: ["Info.plist"]),
+            exclude: ["Info.plist"],
+            resources:[.process("HermesSimpleAlertView.xib")]),
         .testTarget(
             name: "HermesAlertExamplesUITests",
             dependencies: ["HermesAlert"],
