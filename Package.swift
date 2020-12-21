2208e223322279881fcbcb11574d214d5caf0ddd
Alessandro Marcon <a.marcon@vetrya.com>
2020-12-21 17:58:26 +0100

    SPM
    Removed info.plist from package sent with spam

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HermesAlert",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Info.plist"]),
         .testTarget(
             name: "HermesAlertTests",
-            dependencies: ["HermesAlert"]),
+            dependencies: ["HermesAlert"],
+            exclude: ["Info.plist"]),
     ]
 )
