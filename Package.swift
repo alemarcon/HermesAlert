130fbf867e128c204b822db7323ca3e305c916fa
Alessandro Marcon <a.marcon@vetrya.com>
2020-12-21 18:38:32 +0100

    Deplyment target
    Fixed xib deployment target version

--- Package.swift
+++ Package.swift
@@ -20,11 +20,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HermesAlert",
-            dependencies: [],
+            path: "Source",
             exclude: ["Info.plist"]),
         .testTarget(
-            name: "HermesAlertTests",
+            name: "HermesAlertExamplesUITests",
             dependencies: ["HermesAlert"],
+            path: "HermesAlertExamplesUITests",
             exclude: ["Info.plist"]),
     ]
 )
